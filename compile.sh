3c9d6456f514c6bd5a31a367e28f75202ae00172
googolplex <gglplxxx@gmail.com>
2021-10-21 12:13:48 +0300

    Fix compile script

--- compile.sh
+++ compile.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")" ;
 rm $(find bin -name "*.class") 2>/dev/null ;
 mkdir bin 2>/dev/null ;
 javac -s src/ -d bin/ -cp "$(cat .classpath)" $(find src -name "*.java") &&
-# cd bin && jar xf pokemon.jar && cd .. &&
-jar cfm bin/app.jar MANIFEST.mf $(cd bin/ && find . -name "*.class" -printf "-C bin/ %h/%f ") &&
+cd bin && jar xf pokemon.jar && cd .. &&
+cd bin && jar cfm app.jar ../MANIFEST.mf $(find . -name "*.class") &&
 echo "Compiled successfully, app.jar and *.class files are stored in bin/ directory." ;
